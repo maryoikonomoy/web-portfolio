d623e3952d1ea94c4a2c9e95623c58b1fc16ee8d
Oikon <oikonomoum75@gmail.com>
2024-12-23 14:21:47 +0200

    Idea, assets, alignment

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const title = document.querySelector("h1");
     const typingElement = document.getElementById("typing-text");
-    const text = "I make pixels pretty and code behave... most of the time";
+    const text = `< I make pixels pretty and code behave...most of the time />`;
+    
     let index = 0;
 
     // title
     title.style.opacity = "0";
     title.style.transform = "translateY(-50px)";
+
 
     setTimeout(() => {
         title.style.transition = "all 1.5s ease";
